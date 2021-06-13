7e47bbbe14f24f034dee555f579e669cf234a153
nateraw <nxr9266@g.rit.edu>
2021-06-12 19:03:51 -0600

    :lipstick: style

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import pytorch_lightning as pl
 from data import CocoDataModule
 from model import DetrFinetuner
 
-
-if __name__=='__main__':
+if __name__ == "__main__":
     pl.seed_everything(42)
     dm = CocoDataModule()
     model = DetrFinetuner()
     trainer = pl.Trainer(gpus=1, precision=16, max_steps=300, gradient_clip_val=0.1)
     trainer.fit(model, dm)
-    model.save_pretrained('./balloon_model')
-    dm.feature_extractor.save_pretrained('./balloon_model')
+    model.save_pretrained("./balloon_model")
+    dm.feature_extractor.save_pretrained("./balloon_model")
--- model.py
+++ model.py
@@ -1,23 +1,28 @@
 import pytorch_lightning as pl
-from transformers import DetrForObjectDetection
 import torch
+from transformers import DetrForObjectDetection
+
 
 class DetrFinetuner(pl.LightningModule):
-
-    def __init__(self, model_name_or_path: str = 'facebook/detr-resnet-50', num_labels: int = 1, lr: float = 1e-4, lr_backbone: float = 1e-5, weight_decay: float = 1e-4):
+    def __init__(
+        self,
+        model_name_or_path: str = "facebook/detr-resnet-50",
+        num_labels: int = 1,
+        lr: float = 1e-4,
+        lr_backbone: float = 1e-5,
+        weight_decay: float = 1e-4,
+    ):
         super().__init__()
         self.save_hyperparameters()
 
         self.model = DetrForObjectDetection.from_pretrained(self.hparams.model_name_or_path)
         self.model.config.num_labels = self.hparams.num_labels
-        self.model.class_labels_classifier = torch.nn.Linear(
-            self.model.config.d_model, self.hparams.num_labels + 1
-        )
+        self.model.class_labels_classifier = torch.nn.Linear(self.model.config.d_model, self.hparams.num_labels + 1)
 
         self.forward = self.model.forward
         self.save_pretrained = self.model.save_pretrained
 
-    def shared_step(self, batch, mode='train'):
+    def shared_step(self, batch, mode="train"):
         outputs = self(**batch)
         self.log(f"{mode}_loss", outputs.loss)
         self.log_dict({f"{mode}_{k}": v for k, v in outputs.loss_dict.items()})
